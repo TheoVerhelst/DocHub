84752f4255756d6dfae6d4dd87dff9f3ee6320e5
TheoVerhelst <tverhels@ulb.ac.be>
2017-02-20 15:10:43 +0100

    Reorganize consumers.py, use better names

--- chat/consumers.py
+++ chat/consumers.py
@@ -7,30 +7,7 @@ from django.shortcuts import get_object_or_404
 from channels.auth import channel_session_user_from_http, channel_session_user
 from .models import Message
 
-
-def message_consumer(message):
-    """Handles a websocket message by saving it and broadcasting it to all
-    connected users.
-
-    Connected to "chat-messages".
-    """
-    # Save the message in the database
-    group_slug = message.content['group']
-    chatMessage = Message.objects.create(
-        user=message.content['user'],
-        group=get_object_or_404(catalog.models.Group, slug=group_slug),
-        text=message.content['text']
-    )
-    # Send the message to the group (i.e. all users connected on the group chat)
-    channels.Group("chat" + group_slug).send({'text': chatMessage.dump_json()})
-
-def send_previous_messages(reply_channel, group_slug, limit=20):
-    """Sends previous messages of the group chat to a user. It is intended to
-    be used when a user connects.
-    """
-    messages = Message.objects.filter(group__slug=group_slug)[:limit]
-    for message in messages:
-        reply_channel.send({'text': message.dump_json()})
+# Websockets
 
 @channel_session_user_from_http
 def ws_connect(message):
@@ -49,11 +26,17 @@ def ws_connect(message):
         # Reply with an ACK
         message.reply_channel.send({'accept': True})
 
+        # Add the user to the chat group
         channels.Group("chat" + group_slug).add(message.reply_channel)
-        send_previous_messages(message.reply_channel, group_slug)
+
+        # And also add the user to a group with only him inside, in order
+        # to send him previous messages of the chat (so that only him receive
+        # these previous messages)
+        channels.Group("user" + str(message.user.id)).add(message.reply_channel)
+        send_previous_messages(channels.Group("user" + str(message.user.id)), group_slug)
 
 @channel_session_user
-def ws_message(message):
+def ws_receive(message):
     """Handles a websocket message by putting it on the message channel.
     The message channel is separated from websocket.message so that the sending
     process/consumer can move on immediately and not spend time waiting for the
@@ -62,7 +45,7 @@ def ws_message(message):
     Connected to "websocket.receive".
     """
     # Stick the message onto the processing queue
-    channels.Channel("chat-messages").send({
+    channels.Channel("chat.messages").send({
         'group': message.channel_session['group'],
         'text': message['text'],
         'user': message.user
@@ -75,3 +58,32 @@ def ws_disconnect(message):
     Connected to "websocket.disconnect".
     """
     channels.Group("chat" + message.channel_session['group']).discard(message.reply_channel)
+    channels.Group("user" + str(message.user.id)).discard(message.reply_channel)
+
+
+# Chat
+
+def chat_receive(message):
+    """Handles a websocket message by saving it and broadcasting it to all
+    connected users.
+
+    Connected to "chat.receive".
+    """
+    # Save the message in the database
+    group_slug = message.content['group']
+    chatMessage = Message.objects.create(
+        user=message.content['user'],
+        group=get_object_or_404(catalog.models.Group, slug=group_slug),
+        text=message.content['text']
+    )
+    # Send the message to the group (i.e. all users connected on the group chat)
+    channels.Group("chat" + group_slug).send({'text': chatMessage.dump_json()})
+
+def send_previous_messages(channels_group, group_slug, limit=20):
+    """Sends previous messages of the group chat to a user. It is intended to
+    be used when a user connects.
+    """
+    messages = Message.objects.filter(group__slug=group_slug)[:limit]
+    for message in messages:
+        channels_group.send({'text': message.dump_json()})
+
--- www/routing.py
+++ www/routing.py
@@ -1,9 +1,9 @@
 from channels.routing import route
-from chat.consumers import ws_message, ws_connect, ws_disconnect, message_consumer
+from chat.consumers import ws_receive, ws_connect, ws_disconnect, chat_receive
 
 channel_routing = [
     route("websocket.connect", ws_connect),
-    route("websocket.receive", ws_message),
+    route("websocket.receive", ws_receive),
     route("websocket.disconnect", ws_disconnect),
-    route("chat-messages", message_consumer),
+    route("chat.receive", chat_receive),
 ]
