e5f12982e0f588f347f0a294339e5fcccc87448e
TheoVerhelst <tverhels@ulb.ac.be>
2017-02-25 15:21:50 +0100

    Try to make pad preview effective

---
+++ static/scripts/pad.js
@@ -0,0 +1,26 @@
+var socket = null;
+
+function receiveMessage(message)
+{
+}
+
+function sendMessage(clickEvent)
+{
+}
+
+function initPad()
+{
+    makePreview({})
+}
+
+function makePreview(event)
+{
+    var markdown_text = $("#id_text").val()
+    $("#preview-text").html(markdown.Markdown.toHTML(markdown_text))
+}
+
+$(document).ready(initPad);
+
+$("#preview-tab").click(makePreview);
+
+$("#preview-text").html(markdown.Markdown.toHTML("#Coucou"))
