c0f6d1fd098c1294751ded21a33f2e3a787da5fe
TheoVerhelst <tverhels@ulb.ac.be>
2017-03-21 15:22:13 +0100

    fix

--- www/config/dev.py
+++ www/config/dev.py
@@ -14,7 +14,7 @@ from www.config.default import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["dochub.ddns.be", "127.0.0.1"]
+ALLOWED_HOSTS = ["dochub.ddns.net", "127.0.0.1"]
 
 INSTALLED_APPS += (
     'django_extensions',
